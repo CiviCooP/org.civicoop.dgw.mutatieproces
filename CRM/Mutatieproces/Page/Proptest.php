a20f090c9ed80fc0b212ace7a0c3f3484e235d68
Erik Hommel <hommel@ee-atwork.nl>
2014-01-06 18:45:30 +0100

    work in progress - class Property, tested function create

--- CRM/Mutatieproces/Page/Proptest.php
+++ CRM/Mutatieproces/Page/Proptest.php
@@ -11,10 +11,27 @@ class CRM_Mutatieproces_Page_Proptest extends CRM_Core_Page {
         'vge_id'            =>  1015,
         'complex_id'        =>  1300,
         'subcomplex'        =>  "SC1300A",
-        'vge_street_name'   =>  "Ambachtstraat"
+        'vge_street_name'   =>  "Ambachtstraat",
+        'vge_street_number' =>  21,
+        'vge_city'          =>  "Brummen",
+        'vge_street_unit'   =>  "bis",
+        'vge_postal_code'   =>  "6971 BN",
+        'vge_country_id'    =>  1152,
+        'vge_address_id'    =>  52171,
+        'epa_label'         =>  "A",
+        'epa_pre'           =>  "B",
+        'city_region'       =>  "Centrum",
+        'block'             =>  "Zuid",
+        'vge_type_id'       =>  1,
+        'strategy_label'    =>  "Continueren 1",
+        'strategy_b_pnts'   =>  2,
+        'strategy_c_pnts'   =>  3,
+        'number_rooms'      =>  5,
+        'outside_code'      =>  "Enorme tuin",
+        'stairs'            =>  1,
+        'square_mtrs'       =>  124.5
     );
     $property->create($params);
-   
     //$this->assign('meldingen', $melding);
 
     parent::run();
--- CRM/Property.php
+++ CRM/Property.php
@@ -10,8 +10,8 @@
  * Licensed to De Goede Woning under the Academic Free License version 3.0.
  */
 class Property {
-    private $_table;
-    private $_type_table;
+    private $_table = "";
+    private $_type_table = "";
     public $id = 0;
     public $vge_id = 0;
     public $complex_id = 0;
@@ -21,7 +21,7 @@ class Property {
     public $vge_street_unit = "";
     public $vge_postal_code = "";
     public $vge_city = "";
-    public $vge_coutry_id = 0;
+    public $vge_country_id = 0;
     public $vge_address_id = 0;
     public $epa_label = "";
     public $epa_pre = "";
@@ -39,8 +39,8 @@ class Property {
      * constructor
      */
     function __construct() {
-        $this->_table = "civicrm_property";
-        $this->_type_table = "civicrm_property_type";
+        $this->_table = 'civicrm_property';
+        $this->_type_table = 'civicrm_property_type';
     }
     /**
      * function to add a property
@@ -64,12 +64,15 @@ class Property {
         $query_fields = $this->_setPropertyFields($params);
         if (!empty($query_fields)) {
             $query = "INSERT INTO ".$this->_table." SET ".implode(", ", $query_fields);
-            CRM_Core_Error::debug('query', $query);
-            $resultAdd = CRM_Core_DAO::executeQuery($query);
-            CRM_Core_Error::debug('result',$resultAdd);
+            CRM_Core_DAO::executeQuery($query);
         }
-        
-        
+        $latest_query = "SELECT MAX(id) AS max_id FROM ".$this->_table;
+        $dao_latest = CRM_Core_DAO::executeQuery($latest_query);
+        if ($dao_latest->fetch()) {
+            if (isset($dao_latest->max_id)) {
+                $this->id = $dao_latest->max_id;
+            }
+        }        
     }
     /**
      * function to set fields based on incoming params
@@ -87,125 +90,144 @@ class Property {
             if (!is_integer($params['vge_id'])) {
                 throw new Exception("Parameter vge_id has to be numeric");
             }
-            $result[] = "vge_id = {$params['vge_id']}";
+            $this->vge_id = $params['vge_id'];
+            $result[] = "vge_id = {$this->vge_id}";
         }
         
         if (isset($params['complex_id'])) {
             if (!is_integer($params['complex_id'])) {
                 throw new Exception("Parameter complex_id has to be numeric");
             }
-            $result[] = "complex_id = {$params['complex_id']}";
+            $this->complex_id = $params['complex_id'];
+            $result[] = "complex_id = {$this->complex_id}";
         }
         
         if (isset($params['subcomplex'])) {
-            $subcomplex = CRM_Core_DAO::escapeString($params['subcomplex']);
-            $result[] = "subcomplex = '$subcomplex'";
+            $this->subcomplex = CRM_Core_DAO::escapeString($params['subcomplex']);
+            $result[] = "subcomplex = '{$this->subcomplex}'";
         }
         
         if (isset($params['vge_street_name'])) {
-            $vge_street_name = CRM_Core_DAO::escapeString($params['vge_street_name']);
-            $result[] = "subcomplex = '$vge_street_name'";
+            $this->vge_street_name = CRM_Core_DAO::escapeString($params['vge_street_name']);
+            $result[] = "vge_street_name = '{$this->vge_street_name}'";
         }
 
         if (isset($params['vge_street_number'])) {
             if (!is_integer($params['vge_street_number'])) {
                 throw new Exception("Parameter vge_street_number has to be numeric");
             }
-            $result[] = "vge_street_number = {$params['vge_street_number']}";
+            $this->vge_street_number = $params['vge_street_number'];
+            $result[] = "vge_street_number = {$this->vge_street_number}";
         }
         
         if (isset($params['vge_street_unit'])) {
-            $vge_street_unit = CRM_Core_DAO::escapeString($params['vge_street_unit']);
-            $result[] = "vge_street_unit = '$vge_street_unit'";
+            $this->vge_street_unit = CRM_Core_DAO::escapeString($params['vge_street_unit']);
+            $result[] = "vge_street_unit = '{$this->vge_street_unit}'";
         }
         
         if (isset($params['vge_postal_code'])) {
-            $vge_postal_code = self::formatPostalCode($params['vge_postal_code']);
-            $result[] = "vge_postal_code = '$vge_postal_code'";
+            $this->vge_postal_code = self::formatPostalCode($params['vge_postal_code']);
+            $result[] = "vge_postal_code = '{$this->vge_postal_code}'";
         }
         
         if (isset($params['vge_city'])) {
-            $vge_city = CRM_Core_DAO::escapeString($params['vge_city']);
-            $result[] = "vge_city = '$vge_city'";
+            $this->vge_city = CRM_Core_DAO::escapeString($params['vge_city']);
+            $result[] = "vge_city = '{$this->vge_city}'";
         }
         
         if (isset($params['vge_country_id'])) {
             if (!self::validCountryId($params['vge_country_id'])) {
                 throw new Exception("Vge_country_id {$params['vge_country_id']} is not valid");
             }
-            $result[] = "vge_country_id = {$params['vge_country_id']}";
+            $this->vge_country_id = $params['vge_country_id'];
+            $result[] = "vge_country_id = {$this->vge_country_id}";
         }
         
         if (isset($params['vge_address_id'])) {
             if (!self::validAddressId($params['vge_address_id'])) {
                 throw new Exception("Vge_address_id {$params['vge_address_id']} is not valid");
             }
-            $result[] = "vge_address_id = {$params['vge_address_id']}";
+            $this->vge_address_id = $params['vge_address_id'];
+            $result[] = "vge_address_id = {$this->vge_address_id}";
         }
         
         if (isset($params['epa_label'])) {
-            $epa_label = CRM_Core_DAO::escapeString($params['epa_label']);
-            $result[] = "epa_label = '$epa_label'";
+            $this->epa_label = CRM_Core_DAO::escapeString($params['epa_label']);
+            $result[] = "epa_label = '{$this->epa_label}'";
         }
         
         if (isset($params['epa_pre'])) {
-            $epa_pre = CRM_Core_DAO::escapeString($params['epa_pre']);
-            $result[] = "epa_pre = '$epa_pre'";
+            $this->epa_pre = CRM_Core_DAO::escapeString($params['epa_pre']);
+            $result[] = "epa_pre = '{$this->epa_pre}'";
         }
         
         if (isset($params['city_region'])) {
-            $city_region = CRM_Core_DAO::escapeString($params['city_region']);
-            $result[] = "city_region = '$city_region'";
+            $this->city_region = CRM_Core_DAO::escapeString($params['city_region']);
+            $result[] = "city_region = '{$this->city_region}'";
         }
         
         if (isset($params['block'])) {
-            $block = CRM_Core_DAO::escapeString($params['block']);
-            $result[] = "block = '$block'";
+            $this->block = CRM_Core_DAO::escapeString($params['block']);
+            $result[] = "block = '{$this->block}'";
         }
         
         if (isset($params['vge_type_id'])) {
-            if (!self::validTypeId($params['vge_type_id'])) {
+            if (!$this->validTypeId($params['vge_type_id'])) {
                 throw new Exception("Vge_type_id {$params['vge_type_id']} is not valid");
             }
-            $result[] = "vge_type_id = {$params['vge_type_id']}";
+            $this->vge_type_id = $params['vge_type_id'];
+            $result[] = "vge_type_id = {$this->vge_type_id}";
         }
 
         if(isset($params['strategy_label'])) {
-            $strategy_label = CRM_Core_DAO::escapeString($params['strategy_label']);
-            $result[] = "strategy_label = '$strategy_label'";
+            $this->strategy_label = CRM_Core_DAO::escapeString($params['strategy_label']);
+            
+            $result[] = "strategy_label = '{$this->strategy_label}'";
         }
         
          if (isset($params['strategy_b_pnts'])) {
             if (!is_integer($params['strategy_b_pnts'])) {
                 throw new Exception("Parameter strategy_b_pnts has to be numeric");
             }
-            $result[] = "strategy_b_pnts = {$params['strategy_b_pnts']}";
+            $this->strategy_b_pnts = $params['strategy_b_pnts'];
+            $result[] = "strategy_b_pnts = {$this->strategy_b_pnts}";
         }
         
         if (isset($params['strategy_c_pnts'])) {
             if (!is_integer($params['strategy_c_pnts'])) {
                 throw new Exception("Parameter strategy_c_pnts has to be numeric");
             }
-            $result[] = "strategy_c_pnts = {$params['strategy_c_pnts']}";
+            $this->strategy_c_pnts = $params['strategy_c_pnts'];
+            $result[] = "strategy_c_pnts = {$this->strategy_c_pnts}";
         }
 
          if (isset($params['number_rooms'])) {
             if (!is_integer($params['number_rooms'])) {
                 throw new Exception("Parameter number_rooms has to be numeric");
             }
-            $result[] = "number_rooms = {$params['number_rooms']}";
+            $this->number_rooms = $params['number_rooms'];
+            $result[] = "number_rooms = {$this->number_rooms}";
         }
 
-        if(isset($params['outside_code'])) {
-            $outside_code = CRM_Core_DAO::escapeString($params['outside_code']);
-            $result[] = "outside_code = '$outside_code'";
+        if (isset($params['outside_code'])) {
+            $this->outside_code = CRM_Core_DAO::escapeString($params['outside_code']);
+            $result[] = "outside_code = '{$this->outside_code}'";
         }
         
-         if (isset($params['square_mtrs'])) {
+        if (isset($params['stairs'])) {
+            if ($params['stairs'] != 0 && $params['stairs'] != 1) {
+                throw new Exception("Parameter stairs can only be 0 or 1");
+            }
+            $this->stairs = $params['stairs'];
+            $result[] = "stairs = '{$this->stairs}'";
+        }
+        
+        if (isset($params['square_mtrs'])) {
             if (!is_numeric($params['square_mtrs'])) {
                 throw new Exception("Parameter square_mtrs has to be numeric");
             }
-            $result[] = "square_mtrs = {$params['square_mtrs']}";
+            $this->square_mtrs = $params['square_mtrs'];
+            $result[] = "square_mtrs = {$this->square_mtrs}";
         }
        
         return $result;
@@ -219,7 +241,7 @@ class Property {
      * @return string $formatted_postal_code
      */
     static function formatPostalCode($postal_code) {
-        $formatted_postal_code = "";
+        $formatted_postal_code = $postal_code;
         return $formatted_postal_code;
     }
     /**
@@ -236,11 +258,61 @@ class Property {
         }
         try {
             $api_address = civicrm_api3('Address', 'Getcount', array('id' => $address_id));
-            $count_address = $api_address['result'];
+            $count_address = $api_address;
         } catch (CiviCRM_API3_Exception $e) {
             $count_address = 0;
         }
         if ($count_address == 0) {
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+    /**
+     * static function to check if the country_id exists in CiviCRM
+     * 
+     * @author Erik Hommel (erik.hommel@civicoop.org
+     * @date 6 Jan 2014
+     * @param type $country_id
+     * @return boolean true or false
+     */
+    static function validCountryId($country_id) {
+        if (!is_integer($country_id)) {
+            return FALSE;
+        }
+        try {
+            $api_country = civicrm_api3('Country', 'Getcount', array('id' => $country_id));
+            $count_country = $api_country;
+        } catch (CiviCRM_API3_Exception $e) {
+            $count_country = 0;
+        }
+        if ($count_country == 0) {
+            return FALSE;
+        } else {
+            return TRUE;
+        }
+    }
+    /**
+     * static function to check if the vge_type_id exists
+     * 
+     * @author Erik Hommel (erik.hommel@civicoop.org
+     * @date 6 Jan 2014
+     * @param type $vge_type_id
+     * @return boolean true or false
+     */
+    private function validTypeId($vge_type_id) {
+        if (!is_integer($vge_type_id)) {
+            return FALSE;
+        }
+        $count_query = "SELECT COUNT(*) AS type_count FROM ".$this->_type_table." WHERE id = $vge_type_id";
+        $count_type = 0;
+        $dao_type = CRM_Core_DAO::executeQuery($count_query);
+        if ($dao_type->fetch()) {
+            if (isset($dao_type->type_count)) {
+                $count_type = $dao_type->type_count;
+            }
+        }
+        if ($count_type == 0) {
             return FALSE;
         } else {
             return TRUE;
